4a401421f6af5bce47b299c90ee43624a5ca4b96
cloud-art <artyushenko.k.r1@gmail.com>
2023-05-07 15:26:22 +0300

    dropdown fixed

--- src/components/UI/Dropdown/components/Content/index.tsx
+++ src/components/UI/Dropdown/components/Content/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useInsertionEffect, useRef, useState } from 'react'
 import s from './index.module.scss'
 import classNames from 'classnames'
 
@@ -8,14 +8,28 @@ interface ContentProps {
     style?: React.CSSProperties;
 }
 
+
+
 const Content: React.FC<React.PropsWithChildren<ContentProps>> = ({
     children,
     isOpen,
     className,
     style,
 }) => {
+    const [gluedRight, setGluedRight] = useState(false)
+    const dropdownRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const offsetWidth = dropdownRef.current?.offsetWidth || 0
+        const leftPosition = dropdownRef.current?.getBoundingClientRect().left || 0
+        const documentWidth = document.documentElement.scrollWidth
+        if (offsetWidth + leftPosition > documentWidth) {
+            setGluedRight(true)
+        }
+    }, [])
+
     return (
-        <div className={classNames(s.dropdown, isOpen && s.dropdownOpen, className)} style={style}>
+        <div className={classNames(s.dropdown, isOpen && s.dropdownOpen, className, gluedRight && s.dropdownRight)} style={style} ref={dropdownRef}>
             {children}
         </div>
     )
