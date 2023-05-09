62d6cc21aa942844190466ab25d12b3598f1a723
cloud-art <artyushenko.k.r1@gmail.com>
2023-05-10 01:46:30 +0300

    created footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import s from './index.module.scss';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { HOMEPAGE_ROUTE } from '../../constants/routes';
 
-const Footer = () => {
+interface FooterProps { }
+
+const Footer: React.FunctionComponent<FooterProps> = () => {
+    const items = [
+        {
+            href: HOMEPAGE_ROUTE,
+            text: 'Главная'
+        },
+    ];
+
     return (
-        <div>Footer</div>
-    )
-}
+        <footer className={s.footer}>
+            <div className={classNames('container', s.container)}>
+                <ul className={s.list}>
+                    {items.map((el) => {
+                        return (
+                            <li key={el.text} className={s.item}>
+                                <Link to={el.href} className={s.link}>
+                                    {el.text}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <span className={s.author}>by @cloud-art</span>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
